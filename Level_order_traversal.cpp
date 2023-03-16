711bcbeedb8c851b3a35feac3048478726808903
HanzalaKabir <fahadkabeer13@gmail.com>
2023-03-16 14:53:52 +0530

    16-03-2023

--- Level_order_traversal.cpp
+++ Level_order_traversal.cpp
@@ -20,31 +20,31 @@ struct node
         left = right = NULL;
     }
 };
-void Distance(node *root, int k)
+void printLevel(node *root)
 {
     if (root == NULL)
     {
         return;
     }
-    if (k == 0)
+    queue<node *> q;
+    q.push(root);
+    while (q.empty() == false)
     {
-        cout << root->data << " ";
-        return;
-    }
-    else
-    {
-        Distance(root->left, k - 1);
-        Distance(root->right, k - 1);
+        node *curr = q.front();
+        q.pop();
+        cout << curr->data << " ";
+        if (curr->left != NULL)
+        {
+            q.push(curr->left);
+        }
+        if (curr->right != NULL)
+        {
+            q.push(curr->right);
+        }
     }
 }
 int main()
 {
-    struct node *root = new node(10);
-    root->left = new node(6);
-    root->right = new node(8);
-    root->right->right = new node(7);
-    root->right->right->left = new node(11);
-    root->right->right->right = new node(12);
-    Distance(root, 3);
+
     return 0;
 }
--- height_of_binary_tree.cpp
+++ height_of_binary_tree.cpp
@@ -14,21 +14,25 @@ struct node
     int key;
     node *left;
     node *right;
-    node(int k){
-        key=k;
-        left=right=NULL;
+    node(int k)
+    {
+        key = k;
+        left = right = NULL;
     }
 };
-int height(node *root){
-    if(root==NULL){
+int height(node *root)
+{
+    if (root == NULL)
+    {
         return 0;
     }
-    else{
-        return max(height(root->left),height(root->right))+1;
+    else
+    {
+        return max(height(root->left), height(root->right)) + 1;
     }
 }
 int main()
 {
-node *root=NULL;
-return 0;
+    node *root = NULL;
+    return 0;
 }
